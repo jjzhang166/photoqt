3b85fd86590b1ca0f8ebdc785bb09d4165198a28
luspi <Lukas@photoqt.org>
2013-07-31 22:34:18 +0100

    Adding some licensing stuff to make it all a bit clearer (the license of course didn't change, it's still GPLv2 or later)

--- main.cpp
+++ main.cpp
@@ -1,3 +1,26 @@
+/*
+ *
+ * PhotoQt is a simple image viewer, designed to be good looking,
+ * highly configurable, yet easy to use and fast.
+ *
+ * Copyright (C) 2013, Lukas Spies (Lukas@photoqt.org)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
 #include <QtGui>
 #include "mainwindow.h"
 #include <iostream>
@@ -12,7 +35,7 @@ int main(int argc, char *argv[]) {
 	QString globVersion = QString::fromStdString(VERSION);
 
 	// A help message for the command line
-	QString hlp = "\nPhotoQt v" + globVersion + " - created by Lukas Spies (photoQt@ymail.com) - License: GPL\n";
+	QString hlp = "\nPhotoQt v" + globVersion + " -  Copyright (C) 2013, Lukas Spies (Lukas@photoqt.org), License: GPLv2 (or later)\n";
 	hlp += "PhotoQt is a fast, simple, good looking, yet powerfull and highly configurable image viewer.\n\n";
 
 	hlp += "Usage: photoqt [options|file]\n\n";
