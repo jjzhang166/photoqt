c86c6e7e189820ea808a8dca0fb86e09b046419a
Lukas Spies <Lukas@photoqt.org>
2014-12-08 11:42:59 -0330

    tiny adjustment of text in about widget

--- widgets/aboutwidget.cpp
+++ widgets/aboutwidget.cpp
@@ -28,7 +28,7 @@ About::About(QWidget *parent) : MyWidget(parent) {
 	license->setWordWrap(true);
 	lay->addWidget(license);
 
-	// The logo (Thanks to Archie Arevalo)
+	// The new logo
 	QLabel *logo = new QLabel;
 	logo->setPixmap(QPixmap(":/img/logo.png"));
 	QHBoxLayout *logoLay = new QHBoxLayout;
@@ -42,7 +42,7 @@ About::About(QWidget *parent) : MyWidget(parent) {
 
 	// The main text block
 	QString txt = tr("PhotoQt is a simple image viewer, designed to be good looking, highly configurable, yet easy to use and fast.") +  "<br><br>";
-	txt += tr("With PhotoQt I try to be a little different than other image viewers (after all, there are plenty of good image viewers out there). Its interface is kept very simple, yet there is an abundance of settings to customize the look and feel to make PhotoQt YOUR image viewer. ") + "<br>";
+	txt += tr("With PhotoQt I try to be different than other image viewers (after all, there are plenty of good image viewers already out there). Its interface is kept very simple, yet there is an abundance of settings to customize the look and feel to make PhotoQt YOUR image viewer. ") + "<br><br>";
 	txt += tr("I'm not a trained programmer. I'm a simple Maths student that loves doing stuff like this. Most of my programming knowledge I taught myself over the past 10-ish years, and it has been developing a lot since I started PhotoQt. During my studies in university I learned a lot about the basics of programming that I was missing. And simply working on PhotoQt gave me a lot of invaluable insights. So the code of PhotoQt might in places not quite be done in the best of ways, but I think it's getting better and better with each release.") + "<br><br>";
 	txt += tr("I heard a number of times people saying, that PhotoQt is a 'copy' of Picasa's image viewer. Well, it's not. In fact, I myself have never used Picasa. I have seen it in use though by others, and I can't deny that it influenced the basic design idea a little. But I'm not trying to do something 'like Picasa'. I try to do my own thing, and to do it as good as I can.");
 	QString txt2 = tr("Don't forget to check out the website:") + " http://PhotoQt.org";
